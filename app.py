ff205b1b623fd04f2919455f1b2f778afd2e17a8
p_rytvinsky <p_rytvinsky@wargaming.net>
2018-11-14 00:03:27 +0300

    try to fix NameError: name 'db' is not defined

--- app.py
+++ app.py
@@ -6,12 +6,15 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemsList
 from resources.store import Store, StoreList
+from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
 app.secret_key = 'zhopa'
+db.init_app(app)
+
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
@@ -27,7 +30,7 @@ api.add_resource(ItemsList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
-if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
-    app.run(debug=True)
+# if __name__ == '__main__':
+
+
+app.run(debug=True)
