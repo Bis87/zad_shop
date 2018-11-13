4e518c46d0783939e766e8eb757cc2a84fdcfb5d
p_rytvinsky <p_rytvinsky@wargaming.net>
2018-11-14 00:18:56 +0300

    try to fix NameError: name 'db' is not defined

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemsList
 from resources.store import Store, StoreList
-from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -29,5 +28,6 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(debug=True)
